70189f23957a020d645f2cd59b527fb5fa41dca4
Eze Anthony Anayo <101370804+Brunchcode@users.noreply.github.com>
2024-04-23 16:27:35 +0000

    completed

--- airflow/dags/extract_dag.py
+++ airflow/dags/extract_dag.py
@@ -1,1 +1,16 @@
+'''Extract DAG'''
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
 
+with DAG(
+    'extract_dag',
+    schedule_interval=None,
+    start_date=datetime(2024,4,23),
+    catchup=False
+) as dag:
+    
+    extract_task = BashOperator(
+        task_id = 'extract_task',
+        bash_command='wget -c https://raw.githubusercontent.com/vinooganesh/hands-on-introduction-data-engineering-4395021-data-files/main/top-level-domain-names.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/airflow-extract-data.csv'
+    )
